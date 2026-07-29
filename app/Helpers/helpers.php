e80df84b2d83da4445b9621a9153faa6d3ea45f3
shakurdevpro-cloud <shakurdevpro@gmail.com>
2026-07-29 02:11:09 +0200

    Helpers creation

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+if (! function_exists('price_format')) {
+
+    function price_format(int $cents): string
+    {
+        return '$' . number_format($cents / 100, 2);
+    }
+}
+
+
+if (! function_exists('generate_reference')) {
+
+    function generate_reference(string $prefix): string
+    {
+        return $prefix
+            . '-' . date('Y')
+            . '-' . str_pad(
+                random_int(1, 9999),
+                4,
+                '0',
+                STR_PAD_LEFT
+            );
+    }
+}
